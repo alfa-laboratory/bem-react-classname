c7338b1efaed3c4a853dfdd8a776dde3bb944d87
Aleksandr Kitov <akitov@alfabank.ru>
2019-12-18 17:30:01 +0300

    fix(create-theme): add forward ref support

--- src/create-theme.tsx
+++ src/create-theme.tsx
@@ -4,15 +4,18 @@ export function createTheme<T>(defaultTheme: T) {
     const ThemeContext = React.createContext<T>(defaultTheme);
 
     function withTheme<P extends { theme?: T }>(
-        Component: React.ComponentClass<P>
+        Component: React.ComponentClass<P>, defaultTheme?: T
     ) {
-        return function ThemedComponent(props: P) {
+        const ForwardRef: React.RefForwardingComponent<React.Component<P>, P> = (props, ref) => {
             return (
                 <ThemeContext.Consumer>
-                    { state => <Component theme={ state } { ...props } /> }
+                    { state => <Component theme={ state || defaultTheme } ref={ ref } { ...props } /> }
                 </ThemeContext.Consumer>
             );
         };
+        ForwardRef.displayName =`ThemedComponent(${Component.displayName || Component.name})`;
+
+        return React.forwardRef(ForwardRef);
     }
 
     return {
