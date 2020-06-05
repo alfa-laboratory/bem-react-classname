2691eb45fc22832d15c6c617e6ccc6a73692bb03
mishenkoa <leshka.mishenko@gmail.com>
2020-06-05 11:16:48 +0500

    hore(*): fix types in withTheme HOC

--- src/create-theme.tsx
+++ src/create-theme.tsx
@@ -3,32 +3,32 @@ import React from 'react';
 export function createTheme<T>(defaultTheme: T) {
     const ThemeContext = React.createContext<T>(defaultTheme);
 
-    function withTheme<P extends { theme?: T }>(
-        Component: React.ComponentType<P>
-    ): React.ForwardRefExoticComponent<React.PropsWithoutRef<P> & React.RefAttributes<React.Component<P>>> {
-        const ForwardRef: React.RefForwardingComponent<React.Component<P>, P> = ({ ...props }, ref) => {
-            return (
-                <ThemeContext.Consumer>
-                    { (state) => {
-                        const propsWithTheme = {
-                            ...props,
-                            theme: props.theme || state || defaultTheme
-                        };
+    function withTheme<P extends { theme?: T }, U extends React.ReactNode>(
+        Component: React.ComponentType<P>,
+    ) {
+        const ThemedComponent: React.RefForwardingComponent<U, P> = ({ ...props }, ref) => (
+            <ThemeContext.Consumer>
+                { (state) => {
+                    const propsWithTheme = {
+                        ...props,
+                        theme: props.theme || state || defaultTheme,
+                    };
 
-                        return (
-                            <Component { ...propsWithTheme } ref={ ref } />
-                        );
-                    } }
-                </ThemeContext.Consumer>
-            );
-        };
-        ForwardRef.displayName =`ThemedComponent(${Component.displayName || Component.name})`;
+                    return (
+                        <Component { ...propsWithTheme } ref={ ref } />
+                    );
+                } }
+            </ThemeContext.Consumer>
+        );
+        
+        ThemedComponent.displayName =`ThemedComponent(${Component.displayName || Component.name})`;
+        const ForwardRef = React.forwardRef<U, P>(ThemedComponent);
 
-        return React.forwardRef(ForwardRef);
+        return ForwardRef as typeof ForwardRef & U;
     }
 
     return {
         ThemeProvider: ThemeContext.Provider,
-        withTheme
+        withTheme,
     };
 }
