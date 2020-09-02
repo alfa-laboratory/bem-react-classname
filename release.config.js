f8c42e9b37e9175b885d7d4da0537c0274ad49a5
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-09-02 11:04:43 +0300

    feat(*): adds new packages

---
+++ release.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/changelog',
+        '@semantic-release/github',
+        '@semantic-release/npm',
+    ],
+    branches: ['master'],
+    repositoryUrl: 'https://github.com/alfa-laboratory/bem-react-classname',
+};
