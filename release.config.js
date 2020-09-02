de772487b949e8373c059aedeff440f8384e72a5
Igor Belyaev <IYBelyaev@alfabank.ru>
2020-09-02 17:13:11 +0300

    fix(*): fixed npm-publish plan

--- release.config.js
+++ release.config.js
@@ -4,7 +4,10 @@ module.exports = {
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
         '@semantic-release/github',
-        '@semantic-release/npm',
+        [
+            '@semantic-release/npm',
+            { pkgRoot: './.publish' }
+        ],
     ],
     branches: ['master'],
     repositoryUrl: 'https://github.com/alfa-laboratory/bem-react-classname',
